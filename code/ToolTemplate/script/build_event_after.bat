7f62ed372496fe27179518088ea2b18dfd1d55cb
echohelper <echohelper@qq.com>
2020-11-06 14:09:48 +0800

    craete project and add tooltemplate program

---
+++ code/ToolTemplate/script/build_event_after.bat
@@ -0,0 +1,39 @@
+@echo off
+rem *****************************************************************
+rem desc: copy project-files from build to bin
+rem note: no blank lines before and after brackets
+rem %1:$(ConfigurationName)  %2:$(SolutionName)  %3:$(ProjectName)
+rem %4:$(OutputType)
+rem start $(ProjectDir)script\build_event_after.bat $(Configuration) $(SolutionName) $(ProjectName) $(OutputType)
+rem author: echohelper
+rem *****************************************************************
+
+echo run script, after build event!
+
+rem use vs macro to set param
+@set ConfigurationName=%1
+@set SolutionName=%2
+@set ProjectName=%3
+@set OutputType=%4
+
+@set SolutionOutput=%SolutionName%_%ConfigurationName%_x64
+
+
+rem CurrentDir is ProjectDir. switch to SolutinDir.
+rem CurrentDir is SolutinDir Now.
+cd ..\..
+
+rem copy runtime files
+if "%1" == "Release" (
+	xcopy /Y /F /E /R build\%SolutionOutput%\%ProjectName%.exe bin\
+) else (
+	xcopy /Y /F /E /R build\%SolutionOutput%\%ProjectName%.exe bin\
+	xcopy /Y /F /E /R build\%SolutionOutput%\%ProjectName%.pdb bin\
+	xcopy /Y /F /E /R build\%SolutionOutput%\%ProjectName%.pdb pdb\
+)
+
+rem debug
+::pause
+
+rem it is need
+exit
